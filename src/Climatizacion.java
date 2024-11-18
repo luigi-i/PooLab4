c72ab4cffbde98022c976aa051fbef2a3fa3345e
Luigi <bar24056@uvg.edu.gt>
2024-11-17 22:29:45 -0600

    Los metodos que no pertenecen a la interfaz implementados Ademas de un .bat con limpieza de consola para que sea mas facil de probar y se vea mejor

--- src/Climatizacion.java
+++ src/Climatizacion.java
@@ -82,7 +82,7 @@ public class Climatizacion implements NivelVentilacion,ControlHumedad,Calefaccio
     }
 
     private static boolean ON = false;
-    private static int ventEspecifica = 0;
+    private static int ventEspecifica = 3;
     private static boolean mute = false;
     private static int tempSistema = 20;
     private static boolean fog = false;
@@ -111,6 +111,12 @@ public class Climatizacion implements NivelVentilacion,ControlHumedad,Calefaccio
         mute = !mute;
     }
 
+    public static String showMute(){
+        if(Climatizacion.mute){ return "ON";}
+        else return "OFF";
+    }
+
+
     public static boolean isFog() {//retorna si el desempanador esta encendido o no
         return fog;
     }
@@ -118,9 +124,23 @@ public class Climatizacion implements NivelVentilacion,ControlHumedad,Calefaccio
         fog = !fog;
     }
 
+    public static String showFog(){
+        if(Climatizacion.fog){ return "Encendido";}
+        else return "Apagado";
+    }
+
+
     public static int getTempSistema() {
         return tempSistema;
     }
+
+    public static void tempUp(){
+        tempSistema += 1;
+    }
+    public static void tempDown(){
+        tempSistema -= 1;
+    }
+
     public static void setTempSistema(int tempSistema) {
         if (tempSistema < 16) {
             Climatizacion.tempSistema = 20;
@@ -136,12 +156,21 @@ public class Climatizacion implements NivelVentilacion,ControlHumedad,Calefaccio
         return ventEspecifica;
     }
     public static void setventEspecifica(int ventEspecifica) {
-        if (ventEspecifica >= 0 && ventEspecifica <= 3) {
+        if (ventEspecifica >= 1 && ventEspecifica <= 3) {
             Climatizacion.ventEspecifica = ventEspecifica;
         }
         else {
-            Climatizacion.ventEspecifica = 0;}
+            Climatizacion.ventEspecifica = 1;}
     }
+
+    public static String showVentEspecifica(){
+        if (Climatizacion.ventEspecifica == 1) { return "Parabrisas";}
+        else if (Climatizacion.ventEspecifica == 2) { return "Pies";}
+        else if (Climatizacion.ventEspecifica == 3) { return "Frontal";}
+        else { return "OFF";}
+    }
+
+
 
     public static int getHumedad() {
         return humedad;
@@ -154,4 +183,7 @@ public class Climatizacion implements NivelVentilacion,ControlHumedad,Calefaccio
 
         }
     }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,75 +3,135 @@ public class Main {
     public static void main(String[] args) {
         Climatizacion clima=new Climatizacion();
         Scanner sc = new Scanner(System.in);
+        boolean seguir = false; //Seguir con el menu principa;;
+        int opcion = 0; //opcion para encender el sistema
+        int opcion2 = 0; //opcion elegida del menu
+        String Msg = ""; //String asignado para mostrar los mensajes
 
-        System.out.println("\nClimatizador\n\n");
-        System.out.println("------------------------");
-        System.out.print("---"+ Climatizacion.showON() + "---" );
-        System.out.println("------------------------");
-        //primera interfaz solo encender
-        System.out.println("Encender?");
-        System.out.println("1.Si / 2.No");
-        int opcion = sc.nextInt();
-        //ya encendido el programa dar las demas interfazes
-        //al seleccionar otra opcion mostrar la interfaz segun la opcion que escoja
-        if( opcion == 1 ) {
-            System.out.println("1.Ajustar grado de temperatura Grado a grado");
-            System.out.println("2.Ajustar temperatura Segun el clima externo");
-            System.out.println("3.Nivel de ventilacion");
-            System.out.println("4.Modo calefaccion");
-            System.out.println("5.Distribucion de aire");
-            System.out.println("6.Desempañador");
-            System.out.println("7.Control de humedad");
-            System.out.println("8.Mantenimiento");
-            int opcion2 = sc.nextInt();
-            switch(opcion2) {
-                case 1:
-                    System.out.println("1.Subir un grado");
-                    System.out.println("2.Bajar un grado");
-                    break;
-                case 2:
-                    System.out.println("Ingresar temperatura del clima externo");
-                    int level = sc.nextInt();
-                    clima.adjustVetn(level);
-                    System.out.println("La temperatura acutal es de"+Climatizacion.getTempSistema());
-                    break;
-                case 3:
-                    System.out.println("Ventilacion");
-                    System.out.println("1.Modos:1.Bajo,2.Medio,3.Alto");
-                    System.out.println("2.Ventilacion Automatica");
-                    System.out.println("3.Ventilacion en Sona especifica");
-                    System.out.println("4.Modo Silencioso");
-                    break;
-                case 4:
-                    System.out.println("1.Calefaccion rapida");
-                    break;
-                case 5:
-                    System.out.println("1.Frontal");
-                    System.out.println("2.Parabrisas");
-                    System.out.println("3.Pies");
-                    break;
-                case 6:
-                    System.out.println("1.Activar");
-                    System.out.println("2.Desactivar");
-                    break;
-                case 7:
-                    System.out.println("1.Nivel de Humedad:1.Bajo,2.Medio,3.Alto");
-                    System.out.println("2.Indicador visual de humedad");
+        System.out.println("\033c"); //limpia la terminal
+        while (opcion != 1) { //mientras no se encienda el sistema se repite este while
+            System.out.println("Presione 1 para encender");
+            try {
+                opcion = sc.nextInt();
+                if (opcion == 1) {
+                    seguir = true;
+                    Climatizacion.setON(); //enciende la climatizacion
+                }
+            } catch (Exception e) {
+                System.out.println("Por favor, para encender presione 1");
             }
-
-
+            sc.nextLine();
+            System.out.println("\033c");
         }
 
-        System.out.println("");
-        System.out.println("3.");
 
-        System.out.print("Temp: xx  ///  ");
-        System.out.println("Direccion: ---  ///  \n");
-        System.out.print("Mute: OFF  ///  ");
-        System.out.print("Fog: OFF  ///  ");
-        System.out.println("Humedad: x");
-        System.out.println("------------------------");
-        System.out.println("\nEspacio para mostrar mensajes de error\n");
+        while(seguir) {
 
+            System.out.println("\033c");
+
+            System.out.println("------------------------\nClimatizador\n");
+            System.out.println("------------------------");
+            System.out.println("---" + Climatizacion.showON() + "---");
+            System.out.print("Temp: " + Climatizacion.getTempSistema() + "  ///  ");
+            System.out.print("Direccion: " + Climatizacion.showVentEspecifica() + "\n");
+            System.out.print("Modo silencioso: Apagado    ///  ");
+            System.out.println("Mute: " + Climatizacion.showMute());
+            System.out.print("Desempañador: " + Climatizacion.showFog() +"    ///  ");
+            System.out.println("Humedad: " + Climatizacion.getHumedad());
+            System.out.println("------------------------");
+            System.out.println("\n" + Msg + "\n");
+
+
+            Msg = "---"; //vuelve a resetear el mensaje de error
+            System.out.println("------------------------");
+            //primera interfaz solo encender
+
+            //ya encendido el programa dar las demas interfazes
+            //al seleccionar otra opcion mostrar la interfaz segun la opcion que escoja
+
+                System.out.println("1.Ajustar grado de temperatura Grado a grado");
+                System.out.println("2.Ajustar temperatura Segun el clima externo"); //pendiente de interfaz
+                System.out.println("3.Nivel de ventilacion"); //pendiente de interfaz
+                System.out.println("4.Modo calefaccion");
+                System.out.println("5.Distribucion de aire");
+                System.out.println("6.Desempañador");
+                System.out.println("7.Control de humedad");
+                System.out.println("8.Mantenimiento");
+                System.out.println("9.Apagar");
+                System.out.println("10. Salir");
+
+                try {
+                    opcion2 = sc.nextInt();
+                } catch (Exception e) {opcion2 = 0 ;}
+
+                sc.nextLine();
+                switch (opcion2) {
+
+                    case 1:
+                        int case1 = 0;
+                        System.out.println("0.Bajar un grado");
+                        System.out.println("1.Subir un grado");
+
+                        try {
+                            case1 = sc.nextInt();
+                            if (case1 == 0) {Msg = "Temperatura disminuida";}
+                            else {Msg = "Temperatura aumentada";}
+
+                        }catch (Exception e) {
+                            case1 = 2;
+                            Msg = "Ingrese 0 o 1 para cambiar la temperatura";
+                        }
+
+                        if(case1 == 0) { Climatizacion.tempDown();}
+                        else if (case1 == 1) { Climatizacion.tempUp();}
+
+                        break;
+                    case 2:
+                        System.out.println("Ingresar temperatura del clima externo");
+                        int level = sc.nextInt();
+                        clima.adjustVetn(level);
+                        System.out.println("La temperatura acutal es de" + Climatizacion.getTempSistema());
+                        break;
+                    case 3:
+                        System.out.println("Ventilacion");
+                        System.out.println("1.Modos:1.Bajo,2.Medio,3.Alto");
+                        System.out.println("2.Ventilacion Automatica");
+                        System.out.println("3.Ventilacion en Sona especifica");
+                        System.out.println("4.Modo Silencioso");
+                        break;
+                    case 4:
+                        System.out.println("1.Calefaccion rapida");
+                        break;
+                    case 5:
+                        System.out.println("Ingrese hacia donde desea dirigir el aire");
+                        System.out.println("1.Parabrisas --- 2.Pies --- 3.Frontal");
+                        int case5 = 0;
+                        try{
+                            case5 = sc.nextInt();
+                            Climatizacion.setventEspecifica(case5);
+                            Msg = "Direccion del aire actualizada";
+                        }catch (Exception e) { Msg = "Ingrese un numero entre 1 o 3 para cambiar la direccion";}
+
+                        sc.nextLine();
+
+                        break;
+                    case 6:
+                        Climatizacion.setFog();
+                        Msg = "Se ha cambiado la configuracion del desempañador a: " + Climatizacion.showFog();
+
+                    case 7:
+                        int case7 = 0;
+
+                        System.out.println("Ingrese el nivel de humedad que desea: 0. Apagado 1.Bajo,2.Medio,3.Alto");
+
+                        try {
+                            case7 = sc.nextInt();
+                            Climatizacion.setHumedad(case7);
+                            Msg = "Humedad actualizada";
+                        }catch (Exception e){
+                            Msg = "Por favor ingrese un numero entre 0 y 3";
+                        }
+                }
+        }
     }
 }
